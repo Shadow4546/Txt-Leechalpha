d20cca9592075f8a461a55f94bbec93751a484ab
Shadow4546 <89007456+Shadow4546@users.noreply.github.com>
2025-02-01 08:38:40 +0530

    vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,6 @@
-API_ID = int(environ.get("API_ID", "28935416"))
-API_HASH = environ.get("API_HASH", "e18c05697d95edfe39d8957f6e110308")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8105056819:AAEbNiQAAijX_bElj5V5zEEAjl_mSQd4tg4") 
+from os import environ
+
+API_ID = int(environ.get("API_ID", "26"))
+API_HASH = environ.get("API_HASH", "e18110308")
+BOT_TOKEN = environ.get("BOT_TOKEN", "81l_mSQd4tg4") 
 
